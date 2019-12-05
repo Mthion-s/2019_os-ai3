e78feb12951023d20fd8e0920ef184bccc93d0d5
Mthion-s <56263689+Mthion-s@users.noreply.github.com>
2019-12-05 13:08:33 +0800

    Add files via upload

---
+++ 171491415谭峻松/NewComparator.java
@@ -0,0 +1,19 @@
+
+/**
+ * 新的比较器
+ */
+import java.util.Comparator;
+ 
+public class NewComparator implements Comparator<Struct> {
+ 
+	@Override
+	public int compare(Struct o1, Struct o2) {
+		if(o1.fvalue>o2.fvalue)
+			return 1;
+		else if(o1.fvalue<o2.fvalue)
+			return -1;
+		else
+			return 0;
+	}
+ 
+}
